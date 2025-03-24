e0d999b97221a71364cb8ec10abd6d5cfcb317f6
Sushanth N <sushanthnandeti96@gmail.com>
2025-03-24 19:17:43 -0400

    Images overlay for obstacles

--- script.js
+++ script.js
@@ -60,9 +60,17 @@ class Obstacle {
         this.collisionX = Math.random() * this.game.width; 
         this.collisionY = Math.random() * this.game.height;
         this.collisionRadius = 60;
+        this.image = document.getElementById("obstacles");
+        this.spriteWidth = 250; 
+        this.spriteHeight = 250; 
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
+        this.spriteX = this.collisionX - this.width * 0.5; 
+        this.spriteY = this.collisionY - this.height * 0.5 - 50; 
     }
 
     draw(context) {
+        context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
         context.beginPath();
         context.arc( this.collisionX,  this.collisionY, this.collisionRadius, 0, Math.PI * 2);
         context.save();             // saves the state of the canvas
@@ -84,7 +92,7 @@ class Game {
             y : this.height * 0.5,
             pressed : false
         }
-        this.numberOfObstacles = 5;
+        this.numberOfObstacles = 2;
         this.obstacles = [];
 
         // ES6 arrow functions automatically inherit the 'this' keyword from the parent class/scope
