acfcce7203357171c4b55cb61f8ef001e1d63f0d
Sushanth N <sushanthnandeti96@gmail.com>
2025-03-24 20:49:50 -0400

    Use different obstacles images using the sprite attributes

--- script.js
+++ script.js
@@ -67,11 +67,14 @@ class Obstacle {
         this.height = this.spriteHeight;
         this.spriteX = this.collisionX - this.width * 0.5; 
         this.spriteY = this.collisionY - this.height * 0.5 - 50; 
+        this.frameX = Math.floor(Math.random() * 4);
+        this.frameY = Math.floor(Math.random() * 3);
         
     }
 
     draw(context) {
-        context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
+                          this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
         context.beginPath();
         context.arc( this.collisionX,  this.collisionY, this.collisionRadius, 0, Math.PI * 2);
         context.save();             // saves the state of the canvas
