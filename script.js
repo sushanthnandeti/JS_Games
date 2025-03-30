7edaee9459fdea730fab312b643eaba6e031851c
Sushanth N <sushanthnandeti96@gmail.com>
2025-03-29 20:33:06 -0400

    Eggs movement physics added

--- script.js
+++ script.js
@@ -158,8 +158,8 @@ class Egg {
         this.spriteHeight = 135;
         this.width = this.spriteWidth;
         this.height = this.spriteHeight; 
-        this.spriteX = this.collisionX - this.width * 0.5; 
-        this.spriteY = this.collisionY - this.height * 0.5 - 30;
+        this.spriteX;
+        this.spriteY;
         
     }
 
@@ -174,6 +174,22 @@ class Egg {
         context.restore();  // restore to the save state above
         context.stroke();
 }
+    }
+
+    update() {
+        this.spriteX = this.collisionX - this.width * 0.5; 
+        this.spriteY = this.collisionY - this.height * 0.5 - 30;
+        let collisionObjects = [this.game.player, ...this.game.obstacles];
+        collisionObjects.forEach(object => {
+            let [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, object);
+            if (collision) {
+                const unit_x = dx / distance; 
+                const unit_y = dy / distance;
+                this.collisionX = object.collisionX + (sumOfRadii + 1) * unit_x; 
+                this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y;
+
+            }
+        })
     }
 }
 class Game {
@@ -236,7 +252,10 @@ class Game {
             //animate the next frame
             ctx.clearRect(0,0,canvas.width, canvas.height);
             this.obstacles.forEach(obstacle => obstacle.draw(context));
-            this.eggs.forEach(egg => egg.draw(context));
+            this.eggs.forEach(egg => {
+                egg.draw(context);
+                egg.update();
+            });
             this.player.draw(context);
             this.player.update();
             this.timer = 0
